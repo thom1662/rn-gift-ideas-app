dc5c26b39f9231ebb758c68235aa00a72589f312
athom1662 <thom1662@algonquinlive.com>
2024-10-10 15:28:35 -0400

    add fab

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import AppNavigator from "./AppNavigator";
 import { PeopleProvider } from "./PeopleContext";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+
       <PeopleProvider>
         <AppNavigator />
       </PeopleProvider>
-    </View>
+
+      </View>
+
+
+    </SafeAreaProvider>
   );
 }
 
--- screens/PeopleScreen.js
+++ screens/PeopleScreen.js
@@ -1,49 +1,71 @@
 import { useNavigation } from "@react-navigation/native";
 import { useContext } from "react";
-import { Button, FlatList, View, Text, SafeAreaView } from "react-native";
+import { Button, FlatList, View, Text, SafeAreaView, StyleSheet } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { FAB } from '@rneui/themed';
 import PeopleContext from "../PeopleContext";
 
 export default function PeopleScreen() {
   const navigation = useNavigation();
-  //   const people = [
-  //     {
-  //       id: "d825796c-4fc1-4879-ad86-048ece613581",
-  //       name: "Tom",
-  //       dob: "2001-01-09",
-  //     },
-  //     {
-  //       id: "b825796d-4fc1-4879-ad86-048ece613582",
-  //       name: "Megan",
-  //       dob: "2001-02-28",
-  //     },
-  //     {
-  //       id: "a825796e-4fc1-4879-ad86-048ece613583",
-  //       name: "Conor",
-  //       dob: "2001-04-28",
-  //     },
-  //   ];
 
   const { people } = useContext(PeopleContext);
 
   return (
-    <SafeAreaProvider>
-      <SafeAreaView>
-        <FlatList
-          data={people}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <View>
-              <Text>{item.name}</Text>
-              <Text>{item.dob}</Text>
-            </View>
-          )}
-        />
-        <Button
-          title="Add Person"
-          onPress={() => navigation.navigate("AddPerson")}
-        />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        style={styles.list}
+        data={people}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.listItem}>
+            <Text>{item.name}</Text>
+            <Text>{item.dob}</Text>
+          </View>
+        )}
+        ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
+      />
+
+      <FAB
+        size='small'
+        title='Add Person'
+        icon={{
+          name: 'add',
+          color: 'white',
+        }}
+        color=""
+        style={styles.fab}
+        onPress={() => navigation.navigate('AddPerson')}
+      />
+
+      
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    //flex: 1,
+  },
+  listItem: {
+    padding: 20,
+    backgroundColor: 'pink',
+    borderRadius: 15,
+  },
+  list: {
+    marginVertical: 10,
+  },
+  fab: {
+    // position: 'absolute',
+    // right: 50,
+    // bottom: 100,
+    marginVertical: 32,
+
+    // Shadow for iOS
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.6,
+    shadowRadius: 4,
+    // Elevation for Android
+    elevation: 5,
+  },
+});
